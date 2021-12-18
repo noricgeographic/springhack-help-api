e04b4106ec4955d7680c12faf2d6ae9d0712a25c
noric <info@noricgepgraphic.com>
2021-12-19 01:27:58 +0900

    spring initializr

---
+++ src/test/java/com/springhack/help/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springhack.help.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
