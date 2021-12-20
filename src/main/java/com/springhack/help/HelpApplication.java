65d1e89a2fd3de5cfbd71e777c423fee3c2b2955
noric <info@noricgepgraphic.com>
2021-12-20 22:06:09 +0900

    DemoApplication->HelpApplication

--- src/main/java/com/springhack/help/HelpApplication.java
+++ src/main/java/com/springhack/help/HelpApplication.java
@@ -1,13 +1,13 @@
-package com.springhack.help.demo;
+package com.springhack.help;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class HelpApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(HelpApplication.class, args);
 	}
 
 }
