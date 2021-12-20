2c0f5fec9e092905302c891c3a90f6c13c7142aa
noric <info@noricgepgraphic.com>
2021-12-21 00:02:28 +0900

    Serviceの最小構成

--- src/main/java/com/springhack/help/controller/LoginController.java
+++ src/main/java/com/springhack/help/controller/LoginController.java
@@ -1,5 +1,7 @@
 package com.springhack.help.controller;
 
+import com.springhack.help.service.LoginService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +10,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/")
 public class LoginController {
 
+    private final LoginService loginService;
+
+    public LoginController(LoginService loginService) {
+        this.loginService = loginService;
+    }
+
     @GetMapping
     public String get() {
-        return "hello!Spring";
+        String login = loginService.login("foo","bar");
+        return login;
     }
 }
---
+++ src/main/java/com/springhack/help/service/LoginService.java
@@ -0,0 +1,10 @@
+package com.springhack.help.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class LoginService {
+    public String login(String mail, String password) {
+        return "OK";
+    }
+}
